8772e75ff5cf6649cc1bb7477f7b9628c1b89efe
Thorben Westerhuys <thorben@opensource.construction>
2024-04-15 00:56:06 +0200

    speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "opensource.construction",
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <SpeedInsights />
     </html>
   );
 }
