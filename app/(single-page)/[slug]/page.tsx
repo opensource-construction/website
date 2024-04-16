5670f8ca88a5ffd7922692f0d76d4bb93255bb4e
Thorben Westerhuys <thorben@opensource.construction>
2024-04-16 23:44:15 +0200

    page component

--- app/(single-page)/[slug]/page.tsx
+++ app/(single-page)/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { getPosts } from "@/components/utils";
-import { CustomMDX } from "@/components/mdx";
 import { notFound } from "next/navigation";
 import Button from "@/components/button";
+import Page from "@/components/page";
 
 export async function generateStaticParams() {
   let posts = getPosts();
@@ -12,19 +12,16 @@ export async function generateStaticParams() {
 }
 
 export default function SinglePage({ params }: { params: { slug: string } }) {
-  let post = getPosts().find((post) => post.slug === params.slug);
+  let page = getPosts().find((page) => page.slug === params.slug);
 
-  if (!post) {
+  if (!page) {
     notFound();
   }
 
   return (
     <div className="m-auto max-w-screen-lg px-4 py-12">
       <Button href="/" label="Go back" type="secondary" icon="left" />
-      <h1 className="mb-8 text-2xl">{post.metadata.title}</h1>
-      <article className="prose-a:text-osc-gray-700 prose prose-a:font-bold prose-a:no-underline hover:prose-a:text-black">
-        <CustomMDX source={post.content} />
-      </article>
+      <Page page={page} />
     </div>
   );
 }
---
+++ components/page.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "./utils";
+import { CustomMDX } from "@/components/mdx";
+
+export default function Page({
+  page,
+}: {
+  page: { metadata: Metadata; slug: string; content: string };
+}) {
+  return (
+    <div>
+      <h1 className="mb-8 text-2xl">{page.metadata.title}</h1>
+      <article className="prose prose-a:font-bold prose-a:text-osc-gray-700 prose-a:no-underline hover:prose-a:text-black">
+        <CustomMDX source={page.content} />
+      </article>
+    </div>
+  );
+}
