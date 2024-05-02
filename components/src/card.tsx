223fc3feaddc4edcd7df44729c873a8333a72f7b
Thorben Westerhuys <thorben@opensource.construction>
2024-05-02 11:23:14 +0200

    refactor events card component

--- components/src/card.tsx
+++ components/src/card.tsx
@@ -1,23 +1,18 @@
 import Button from "./button";
 
-export function EventCard({
+export function Card({
   title,
-  date,
+  subtitle,
   slug,
 }: {
   title: string;
-  date: Date;
+  subtitle: string;
   slug: string;
 }) {
   return (
     <div className="mb-32 bg-white p-5">
       <h4 className="mb-2 mt-0 text-xl">{title}</h4>
-      <div className="mb-12">
-        {Intl.DateTimeFormat("en-UK", {
-          dateStyle: "short",
-          timeZone: "Europe/Zurich",
-        }).format(date)}
-      </div>
+      <div className="mb-12">{subtitle}</div>
       <Button
         label="More about the event"
         href={`/events/${slug}`}
