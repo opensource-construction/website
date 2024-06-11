89a90c89959ee53c4a7b3c654cea5009e588d172
karim-daw <karimdaw91@gmail.com>
2024-06-11 22:27:46 +0100

    Add: fetch component for github markdown

--- app/(single-page)/[pageType]/[slug]/page.tsx
+++ app/(single-page)/[pageType]/[slug]/page.tsx
@@ -6,31 +6,97 @@ type PageType = "events" | "projects" | "trainings" | "faqs";
 type SinglePageType = {
   pageType: PageType;
   slug: string;
+  directMdLink?: string;
 };
+
+type PageProps = {
+  page: {
+    metadata: {
+      title: string;
+      description?: string;
+      event?: object;
+      project?: object;
+      links?: {
+        url: string;
+        label: string;
+      }[];
+      directMdLink?: string;
+    };
+    slug: string;
+    content: string;
+  };
+};
+
+async function fetchPageData(pageType: PageType, slug: string) {
+  let page = getPosts(pageType).find((page) => page.slug === slug);
+
+  if (!page || pageType === "faqs") {
+    return null;
+  }
+
+  if (page.metadata.directMdLink) {
+    try {
+      console.log(
+        `Fetching markdown content from ${page.metadata.directMdLink}`,
+      );
+      const url = new URL(page.metadata.directMdLink);
+      const response = await fetch(url.toString());
+      const content = await response.text();
+      page.content = content;
+    } catch (error) {
+      console.error(
+        `Failed to fetch markdown content from ${page.metadata.directMdLink}:`,
+        error,
+      );
+      return null;
+    }
+  }
+
+  return page;
+}
 
 export async function generateStaticParams() {
   let posts: SinglePageType[] = [];
+  console.log(
+    "Generating static paths for single pages (events, projects, trainings)",
+  );
   posts = [
     ...posts,
     ...getPosts("projects").map((p) => {
-      return { slug: p.slug, pageType: "projects" as PageType };
+      return {
+        slug: p.slug,
+        pageType: "projects" as PageType,
+        directMdLink: p.metadata.directMdLink,
+      };
     }),
     ...getPosts("events").map((p) => {
-      return { slug: p.slug, pageType: "events" as PageType };
+      return {
+        slug: p.slug,
+        pageType: "events" as PageType,
+        directMdLink: p.metadata.directMdLink,
+      };
     }),
     ...getPosts("trainings").map((p) => {
-      return { slug: p.slug, pageType: "trainings" as PageType };
+      return {
+        slug: p.slug,
+        pageType: "trainings" as PageType,
+        directMdLink: p.metadata.directMdLink,
+      };
     }),
   ];
   return posts;
 }
 
-export default function SinglePage({ params }: { params: SinglePageType }) {
-  let page = getPosts(params.pageType).find(
-    (page) => page.slug === params.slug,
-  );
+export default async function SinglePage({
+  params,
+}: {
+  params: SinglePageType;
+}) {
+  const { pageType, slug } = params;
 
-  if (!page || params.pageType === "faqs") {
+  const page = await fetchPageData(pageType, slug);
+
+  if (!page) {
     notFound();
   }
 
