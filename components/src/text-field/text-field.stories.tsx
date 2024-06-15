1a1a5a6e9bef1b25ee1405428e0b4418eaffeb91
Thorben Westerhuys <thorben@opensource.construction>
2024-06-15 11:53:21 +0200

    WIP add text-field and input components

---
+++ components/src/text-field/text-field.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { allModes } from "../../../.storybook/modes";
+import { TextField } from "./text-field";
+
+const meta = {
+  title: "Components/Molecules/TextField",
+  component: TextField,
+  args: {
+    label: "Example label",
+    placeholder: "Example placeholder",
+    type: "text",
+  },
+  argTypes: {},
+} satisfies Meta;
+
+export default meta;
+type Story = StoryObj<typeof TextField>;
+
+export const Default: Story = {
+  parameters: {
+    chromatic: {
+      modes: {
+        xsm: allModes["xsm"],
+        md: allModes["md"],
+        xl: allModes["xl"],
+        "2xl": allModes["2xl"],
+      },
+    },
+  },
+};
+
+export const Email: Story = {
+  args: {
+    label: "E-Mail address",
+    placeholder: "john.doe@example.com",
+    type: "email",
+  },
+};
---
+++ components/src/text-field/text-field.tsx
@@ -0,0 +1,53 @@
+import type { ComponentProps } from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import * as slugify from "slugify";
+
+import { Input } from "../input";
+
+type TextFieldProps = ComponentProps<"input"> &
+  VariantProps<typeof variants> & {
+    label: string;
+    details?: string;
+    type?: "text" | "email";
+  };
+
+export const variants = cva(
+  ["inline-flex items-center gap-1 text-sm font-medium"],
+  {
+    variants: {
+      required: {
+        true: [
+          "after:bg-accent-500 after:h-1.5 after:w-1.5 after:rounded-full",
+        ],
+      },
+    },
+  },
+);
+
+export const TextField = ({
+  label,
+  value,
+  details,
+  placeholder,
+  type = "text",
+  required = false,
+  disabled = false,
+  ...props
+}: TextFieldProps) => {
+  return (
+    <label className="mb-4 flex flex-col gap-1.5">
+      <span className={variants({ required })}>{label}</span>
+
+      <Input
+        id={slugify.default(label)}
+        name={slugify.default(label)}
+        value={value}
+        type={type}
+        placeholder={placeholder}
+        disabled={disabled}
+        {...props}
+      />
+      {details && <span className="text-xs text-slate-500">{details}</span>}
+    </label>
+  );
+};
