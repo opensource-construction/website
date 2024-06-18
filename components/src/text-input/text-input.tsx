c2f4e8707c328f6fea3b01d1be7bd797d001b724
Thorben Westerhuys <thorben@opensource.construction>
2024-06-18 22:18:50 +0200

    design: add form colors to colors

--- components/src/text-input/text-input.tsx
+++ components/src/text-input/text-input.tsx
@@ -9,17 +9,17 @@ type TextInputProps = ComponentProps<"input"> &
 
 export const variants = cva(
   [
-    "form-input transition-all duration-300 appearance-none rounded-md font-bold focus:bg-white focus:border-gray-500 p-5 placeholder:text-gray-400 focus:placeholder:text-gray-200",
+    "form-input transition-all duration-300 appearance-none rounded-md font-bold focus:bg-white focus:border-slate-500 p-5 placeholder:text-slate-400 focus:placeholder:text-slate-200",
   ],
   {
     variants: {
       variant: {
         default: [
-          "border-gray-300 bg-gray-100 hover:bg-white hover:border-gray-500 text-black",
+          "border-slate-500 bg-slate-100 hover:bg-white hover:border-slate-700 text-black",
         ],
-        disabled: ["border-gray-100 bg-gray-50"],
+        disabled: ["border-slate-100 bg-slate-50"],
         required: [
-          "border-gray-300 bg-gray-100 hover:bg-white hover:border-gray-500 text-black",
+          "border-slate-500 bg-slate-100 hover:bg-white hover:border-slate-700 text-black",
         ],
       },
     },
