913eddcd879178f2d204fc935f9ad45956edb470
Thorben Westerhuys <thorben@opensource.construction>
2024-06-19 10:52:55 +0200

    WIP form validation

--- app/actions.ts
+++ app/actions.ts
@@ -4,7 +4,6 @@ import { z } from "zod";
 
 export async function saveForm(route: string, formData: FormData) {
   console.log(formData);
-
   const schema = z.object({
     firstname: z
       .string({
@@ -20,29 +19,30 @@ export async function saveForm(route: string, formData: FormData) {
       .min(2)
       .max(25)
       .trim(),
-    email: z
-      .string({
-        invalid_type_error: "Invalid Email",
-      })
-      .email()
-      .max(35),
-    organisation: z
-      .string({
-        invalid_type_error: "Invalid Organisation",
-      })
-      .min(2)
-      .max(25)
-      .trim(),
+    "e-mail": z.string().email({ message: "Invalid Email" }),
+    "organisation-company": z.optional(
+      z
+        .string({
+          invalid_type_error: "Invalid Organisation",
+        })
+        .max(25)
+        .trim(),
+    ),
+    message: z.optional(z.string().trim()),
+    "accept-toc": z.boolean(),
   });
 
   const validatedFields = schema.safeParse({
     firstname: formData.get("firstname"),
     lastname: formData.get("lastname"),
-    email: formData.get("email"),
-    organisation: formData.get("organisation"),
+    "e-mail": formData.get("e-mail"),
+    "organisation-company": formData.get("organisation-company"),
+    message: formData.get("message"),
+    "accept-toc": formData.get("accept-toc") === "on",
   });
 
   if (!validatedFields.success) {
+    console.error(validatedFields.error.flatten().fieldErrors);
     return {
       errors: validatedFields.error.flatten().fieldErrors,
     };
