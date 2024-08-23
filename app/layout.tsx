06060cb5f8f491a3816969fa2dfc4269a1acec90
Thorben Westerhuys <1425577+n0rdlicht@users.noreply.github.com>
2024-08-23 13:39:35 +0200

    Replace Vercel Analytics with Friendly Analytics / Matomo (#110)
    * feat: Friendly Analytics instead of Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+import Friendly from "@/utils/Friendly";
 import "./globals.css";
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/react";
 import { Footer, Navbar, Button } from "@opensource-construction/components";
 import { corbert, glyphter } from "@/components/src/tokens/fonts";
 
@@ -67,8 +67,8 @@ export default function RootLayout({
         <footer>
           <Footer />
         </footer>
+        <Friendly />
         <SpeedInsights />
-        <Analytics />
       </body>
     </html>
   );
