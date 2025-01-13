9c36dbc696c9e25d57161c5cea604bd1c51eb993
Felix Brunold <48569186+TheVessen@users.noreply.github.com>
2025-01-13 13:50:50 +0100

    Chore/logo display (#160)
    * Refactor partner display to use PartnersPartial component and enhance PartnerCard props
    
    * separate partners into new files
    
    * refactor code
    
    * fix margin at bottom mobile
    
    * add further logos to partner
    
    * Add Felix to team
    
    * fix bldrs logo and links
    
    * restrict hover on mobile
    
    * Add community partner

--- components/src/partnerCard.tsx
+++ components/src/partnerCard.tsx
@@ -1,20 +1,55 @@
-import React from "react";
-
 export type PartnerProps = {
   name: string;
   logo: string;
   url: string;
-  type: string; //"community" | "industry" | "academia"
+  tier: "bronze" | "silver" | "gold" | "network_partner" | "community_partner";
+  type: "community" | "industry" | "academia";
+  size?: "small" | "medium" | "large";
+  className?: string;
 };
-const PartnerCard = (partnerProps: PartnerProps) => {
+
+const sizeClasses = {
+  small: "h-12 sm:h-14",
+  medium: "h-16 sm:h-20",
+  large: "h-24 sm:h-32",
+} as const;
+
+const tierClasses = {
+  gold: "border-2 bg-white sm:hover:shadow-xl border-[hsl(50,80%,60%)] sm:hover:border-[hsl(50,80%,50%)]",
+  silver:
+    "border-2 bg-white sm:hover:shadow-lg border-[hsl(0,0%,75%)] sm:hover:border-[hsl(0,0%,65%)]",
+  bronze:
+    "border-2 bg-white shadow-sm sm:hover:shadow-md border-[hsl(30,60%,60%)] sm:hover:border-[hsl(30,60%,50%)]",
+  network_partner:
+    "border bg-white sm:hover:shadow-sm border-gray-200 sm:hover:border-primary-500",
+  community_partner:
+    "border bg-white sm:hover:shadow-sm border-gray-200 sm:hover:border-primary-500",
+};
+
+const PartnerCard = ({
+  url,
+  logo,
+  name,
+  size = "medium",
+  className = "",
+  tier,
+}: PartnerProps) => {
   return (
-    <a href={partnerProps.url} target="_blank">
-      <div>
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`block  transition-all duration-300 ease-in-out ${tierClasses[tier]} ${className}`}
+    >
+      <div className="flex  flex-col items-center justify-center gap-2 p-4 sm:p-6">
         <img
-          src={`images/partners/${partnerProps.logo}`}
-          alt={partnerProps.name}
-          className="m-6 h-16 min-w-16 max-w-32 object-contain"
+          src={`images/partners/${logo}`}
+          alt={name}
+          className={`object-contain transition-all duration-300 ${sizeClasses[size]}`}
         />
+        {/* <span className="text-gray-600 text-center text-xs opacity-0 transition-opacity duration-300 hover:opacity-100 sm:text-sm">
+          {name}
+        </span> */}
       </div>
     </a>
   );
