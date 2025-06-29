3aac341af327f6776de4da3e3ad6317042886c19
Felix Brunold <48569186+TheVessen@users.noreply.github.com>
2025-06-29 15:09:48 +0200

    Chore/directory (#184)
    * Update project links in layout and page components
    
    * Revise project directory description for clarity and engagement
    
    * Update navigation link for Projects to use internal anchor

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
 };
 
 const navItems = [
-  { name: "Projects", target: "/projects" },
+  { name: "Projects", target: "#project-directory" },
   { name: "Events", target: "/#events" },
   { name: "Trainings", target: "/trainings" },
   { name: "About us", target: "/#who-is-behind-the-initiative" },
